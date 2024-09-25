650b0c1eb617bb426d36e36a0203d1c3d10cab6f
althaafka <althaafka004@gmail.com>
2024-09-25 21:31:34 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const user_data = {
+  "id": "customer@mail.com",
+  "name": "king customer",
+  "image_url": "https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
+}
 
 function App() {
+  const [user, setUser] = useState(user_data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container">
+      <nav class="navbar">
+          <ul>
+              <li>
+                  <img id="user-icon" src={user.image_url} alt="User Icon" class="user-icon"/>
+              </li>
+              <li><i class="icon-chat">C</i></li>
+              <li><i class="icon-settings">S</i></li>
+          </ul>
+      </nav>
+      <div class="menu">
+          menu
+      </div>
+      <div class="chat-area">
+          chat
+      </div>
+      <div class="side-bar">
+          side-bar
+      </div>
+   </div>
   );
 }
 
